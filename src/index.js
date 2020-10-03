ea93a3321cefff9a30a4cc53d67da5c27feca810
Noah Krossa <noahkrossa@gmail.com>
2020-10-03 12:26:25 +0200

    intial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/** Dependencies */
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+/** Use .env settings */
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+/** Initialize express app */
+const app = express()
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Express server running in port ${PORT}`))
