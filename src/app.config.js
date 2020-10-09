e3edd9f21e4c346b2fdaeb6b8e83ff7812fbc52c
Noah Krossa <noahkrossa@gmail.com>
2020-10-09 15:05:44 +0200

    Add secretTOKEN config var

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  mongodbURI: process.env.MONGO_URI || 'mongodb://localhost:27017/my_demo_db'
+  mongodbURI: process.env.MONGO_URI || 'mongodb://localhost:27017/my_demo_db',
+  secretTOKEN: 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
 }
