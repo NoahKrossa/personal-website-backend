c87f106adfaa6466f971e1f145127a48d6da9a0b
Noah Krossa <noahkrossa@gmail.com>
2020-10-09 19:40:08 +0200

    Set config vars (secret token from heroku and adminKey)

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 8080,
   mongodbURI: process.env.MONGO_URI || 'mongodb://localhost:27017/my_demo_db',
-  secretTOKEN: 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+  secretTOKEN: process.env.SECRET_TOKEN || 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+  adminKey: process.env.ADMIN_KEY || '$2y$12$AjYl4dIjz3wY8/.iJd1jZ.p9FMAnYSzr0XURHCNwHQbdG.8s/Esvm'
 }
