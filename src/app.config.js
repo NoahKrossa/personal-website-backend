cf7a67a8b12a61e9edaf9904afb5a103ae4a654e
Noah Krossa <noahkrossa@gmail.com>
2020-10-09 13:47:06 +0200

    change: Change mongodb connection string for local

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  mongodbURI: process.env.MONGO_URI || 'mongodb//localhost:27017/my_demo_db'
+  mongodbURI: process.env.MONGO_URI || 'mongodb://localhost:27017/my_demo_db'
 }
