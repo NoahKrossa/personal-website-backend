7225f8c1c03d7a14b7a9b54fec99dda4a1345163
Noah Krossa <noahkrossa@gmail.com>
2020-10-10 17:38:23 +0200

    Fix user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,17 +4,23 @@ const { createToken } = require('../services/jwt.service')
 const authentificateUser = async (req, res) => {
   try {
     const { username, password } = req.body
-    const user = User.findOne({ name: username })
+    const user = await User.findOne({ name: username }).exec()
+
     if (!user) {
       console.log('Not registred user')
       return res.sendStatus(400)
     }
-    const passwordMatch = await User.passwordMatch(password, user.password)
+
+    const passwordMatch = await User.checkPassword(password, user.password)
     if (!passwordMatch) {
       console.log('Password and username not match')
       return res.sendStatus(400)
     }
     res.cookie('token', createToken(user))
+    res.json({
+      message: 'Logged successfully!',
+      user
+    })
   } catch (e) {
     console.log(e)
     return res.sendStatus(500)
