07db71b272d241e8b548f76fa6aa328333cc2512
Noah Krossa <noahkrossa@gmail.com>
2020-10-16 20:32:26 +0200

    Set express static route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /** Dependencies */
 const express = require('express')
 const helmet = require('helmet')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const config = require('./app.config')
+const { resolve } = require('path')
 
 /** Initialize express app */
 const app = express()
@@ -12,13 +12,20 @@ const app = express()
 /** Using middlewares */
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(helmet())
-app.use(cors())
+
 app.use(cookieParser())
 app.use(logger('dev'))
 
+//Set static
+app.use(express.static(resolve(__dirname, 'dist')))
+
 /** Using APIRouter */
 app.use('/api', require('./api.router'))
+
+// Serve client app
+app.get('/*', (req, res) => {
+  res.sendFile(resolve(__dirname, 'dist', 'index.html'))
+})
 
 /** Connect to mongodb */
 require('./services/mongodb').conntect(config.mongodbURI)
